d4ad5ecf0656807976486687adae145701d0117b
syangtu <hffbe0f0o@gmail.com>
2021-07-01 14:07:48 +0000

    mentor.rb

--- mentor.rb
+++ mentor.rb
@@ -14,16 +14,10 @@ end
 
 class RailsMentor < Mentor
     
-    attr_accessor :name
-    
-    def initialize(name)
-        self.name = name
-    end    
-
     def job
         puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
     end    
-        
+
 end        
 
 mentor = Mentor.new('煌木')
